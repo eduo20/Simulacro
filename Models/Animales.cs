cf9ea5d52e6e7b458fe4ee54e83fccdc909b9786
hp <hp@DESKTOP-LU4K9VA>
2026-05-18 08:34:30 -0600

    Agregar archivos de proyecto.

---
+++ Models/Animales.cs
@@ -0,0 +1,11 @@
+﻿namespace Simulacro.Models
+{
+    public class Animales
+    {
+        public string nombre { get; set; } = string.Empty;
+        public string especie { get; set; } = string.Empty; 
+        public string edad { get; set; } = string.Empty;
+        public string altura {  get; set; } = string.Empty;
+    }
+    }
+}
